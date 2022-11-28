a27ac9f54ed8edc502b50013c249a36b39a0564f
Laura <105425865+Cyber-laura@users.noreply.github.com>
2022-11-28 13:29:58 +0100

    ETAPE11

---
+++ front/js/confirmation.js
@@ -0,0 +1,7 @@
+function main(){
+    let orderIdHtml = document.getElementById("orderId");
+    orderIdHtml.innerText = localStorage.getItem("orderId");
+    console.log(localStorage.getItem("orderId"))
+    localStorage.clear();
+}
+main();
